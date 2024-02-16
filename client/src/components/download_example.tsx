7d6365828b3942ac73200cb9406163b24d6b69bc
rlaclgh <rlaclgh11@gmail.com>
2024-02-16 13:23:00 +0900

    [client] 예시 csv 다운로드

---
+++ client/src/components/download_example.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { CSVLink } from "react-csv";
+
+const csvData = [
+  ["name", "nameEng", "description"],
+  ["제품1", "product1", "제품1 설명"],
+  ["제품2", "product2", "제품2 설명"],
+  ["제품3", "product3", "제품3 설명"],
+  ["제품4", "product4", "제품4 설명"],
+];
+
+const DownloadExample = () => {
+  return (
+    <CSVLink
+      data={csvData}
+      filename={"product_upload_example.csv"}
+      target="_blank"
+    >
+      예시 데이터 다운로드
+    </CSVLink>
+  );
+};
+
+export default DownloadExample;
